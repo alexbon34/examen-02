efce0f1c339a16cbace592e669415bc7f900ff53
Alexander Bonilla <78042528+alexbon34@users.noreply.github.com>
2025-05-23 02:55:51 +0000

    Primer commit h

--- src/Componentes/PaginaPrincipal.tsx
+++ src/Componentes/PaginaPrincipal.tsx
@@ -1,15 +1,16 @@
-import Header from './Header.tsx';
-import AppCards from './AppCards.tsx';
-import AppControlPane from './AppControlPane.tsx';
+import Header from "./Header.tsx";
+import AppCards from "./AppCards.tsx";
+import AppControlPane from "./AppControlPane.tsx";
+import React from "react";
 
-export default function PaginaPrincipal(){
-    return (
-        <>
-            <Header/>
-            <main>
-                <AppCards/>
-                <AppControlPane/>
-            </main>
-        </>
-    )
+export default function PaginaPrincipal() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AppCards />
+        <AppControlPane />
+      </main>
+    </>
+  );
 }
